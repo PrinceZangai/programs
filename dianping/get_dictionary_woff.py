a2eedba6438cfc90221f47b70af56e5cccefa22c
zengxiangjun <ljd1527257@163.com>
2020-11-04 10:57:59 +0800

    '两种字体解密方法'

---
+++ dianping/get_dictionary_woff.py
@@ -0,0 +1,34 @@
+import json
+from fontTools.ttLib import TTFont
+
+a='./font/be9febf2.woff'
+b='./font/2ed8f6f4.woff'
+c='./font/6e3abc4e.woff'
+d='./font/78b94d73.woff'
+font = TTFont(d)
+font.saveXML('test.xml')
+
+        #该步骤为得到字体的按照顺序排列的结果，前两个是没用的，最后的结果和上面的字的列表相对应
+names=font.getGlyphNames()[1:]
+
+
+# print(result)
+chars=[]
+with open('font.txt',encoding='utf-8') as f:
+    while True:
+            char=f.read(1)
+            if char!='\n':
+                chars.append(char)
+            if not char:
+                break
+        # char=f.read(1)
+    # if char!='\n':
+    #     chars.append(char)
+
+
+print(len(names))
+print(len(chars))
+dictionary=dict(zip(names,chars))
+print(dictionary)
+with open('dictionary4.json','w',encoding='utf-8') as f:
+    json.dump(dictionary,f)
